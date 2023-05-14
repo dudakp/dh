d8033cc6a756e1991cce5b22da7a6f5da7cfb76c
dudakp <palodudak@gmail.com>
2023-05-14 12:40:09 +0200

    chore(flow): added tests

--- pkg/flow/flow_test.go
+++ pkg/flow/flow_test.go
@@ -81,41 +81,116 @@ func Test_NewFlow_minHandlers(t *testing.T) {
 	}
 }
 
-// TODO: add more
 func Test_ExecuteEffectFlow_errorPropagation(t *testing.T) {
 	in := "hello"
 	expectedError := errors.New("shit happens")
 
-	handler0 := NewHandler(func(data any) (any, error) {
-		return "", nil
-	}, func(handler *Handler[any], err error) {
-		in = "error"
-	})
-	handler1 := NewHandler(func(data any) (any, error) {
-		return "", nil
-	}, nil)
-	handler2 := NewHandler(func(data any) (any, error) {
-		return nil, expectedError
-	}, nil)
+	//handler0 := NewHandler(func(data any) (any, error) {
+	//	return "", nil
+	//}, func(handler *Handler[any], err error) {
+	//	in = "error"
+	//})
+	//handler1 := NewHandler(func(data any) (any, error) {
+	//	return "", nil
+	//}, nil)
+	//handler2 := NewHandler(func(data any) (any, error) {
+	//	return nil, expectedError
+	//}, nil)
 
-	f, err := NewEffectFlow(&Opts{Name: "TErrorPropagation"}, nil, handler0, handler1, handler2)
-	if err != nil {
-		t.Fatalf("error during creation of flow: %s", err.Error())
-	}
 	tests := []struct {
 		name  string
 		input *EffectFlow[any]
 		want  string
 	}{
 		{
-			name:  "last in flow has error",
-			want:  "error",
-			input: f,
+			name: "last in flow has error",
+			want: "error",
+			input: func() *EffectFlow[any] {
+				f, err := NewEffectFlow(&Opts{Name: "lastInFlowWithErr"}, nil,
+					NewHandler(func(data any) (any, error) {
+						return "", nil
+					}, func(handler *Handler[any], err error) {
+						in = "error"
+					}), NewHandler(func(data any) (any, error) {
+						return "", nil
+					}, nil), NewHandler(func(data any) (any, error) {
+						return nil, expectedError
+					}, nil))
+				if err != nil {
+					t.Fatalf("error during creation of flow: %s", err.Error())
+				}
+				return f
+			}(),
+		},
+		{
+			name: "second-to-last in flow has error",
+			want: "error",
+			input: func() *EffectFlow[any] {
+				f, err := NewEffectFlow(&Opts{Name: "stlInFlowWithError"}, nil,
+					NewHandler(func(data any) (any, error) {
+						return "", nil
+					}, func(handler *Handler[any], err error) {
+						in = "error"
+					}), NewHandler(func(data any) (any, error) {
+						return nil, expectedError
+					}, nil), NewHandler(func(data any) (any, error) {
+						return "", nil
+					}, nil))
+				if err != nil {
+					t.Fatalf("error during creation of flow: %s", err.Error())
+				}
+				return f
+			}(),
+		},
+		{
+			name: "last in flow has error and terminalOnError is defined",
+			want: "global error",
+			input: func() *EffectFlow[any] {
+				f, err := NewEffectFlow(&Opts{Name: "terminalOnError"},
+					func(err error) {
+						in = "global error"
+					}, NewHandler(func(data any) (any, error) {
+						return "", nil
+					}, func(handler *Handler[any], err error) {
+						in = "error"
+					}), NewHandler(func(data any) (any, error) {
+						return "", nil
+					}, nil), NewHandler(func(data any) (any, error) {
+						return "", expectedError
+					}, nil))
+				if err != nil {
+					t.Fatalf("error during creation of flow: %s", err.Error())
+				}
+				return f
+			}(),
+		},
+		{
+			name: "last in flow has error and terminalOnError is defined and option for executing all handers is true",
+			want: "helloglobal errorerror",
+			input: func() *EffectFlow[any] {
+				f, err := NewEffectFlow(&Opts{Name: "terminalOnErrorOverride", ExecuteOnErrorAlways: true},
+					func(err error) {
+						in += "global error"
+					}, NewHandler(func(data any) (any, error) {
+						return "", nil
+					}, func(handler *Handler[any], err error) {
+						in += "error"
+					}), NewHandler(func(data any) (any, error) {
+						return "", nil
+					}, nil), NewHandler(func(data any) (any, error) {
+						return nil, expectedError
+					}, nil))
+				if err != nil {
+					t.Fatalf("error during creation of flow: %s", err.Error())
+				}
+				return f
+			}(),
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
+			in = "hello"
 			err := ExecuteEffectFlow(test.input)
 			if err != nil && !errors.Is(err, expectedError) {
 				t.Errorf("flow ended with error: %s", err.Error())
@@ -125,7 +200,7 @@ func Test_ExecuteEffectFlow_errorPropagation(t *testing.T) {
 				t.Errorf("want: %s, got: %s", test.want, in)
 				t.FailNow()
 			}
-			in = "hello"
+
 		})
 	}
 }
